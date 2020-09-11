c9f0560651eabb362f195fb44952a7760ebda651
Lucas Lej <sacul69@gmail.com>
2020-09-11 17:37:13 +0200

    11 września

---
+++ DevHobby.BLL/Produkt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevHobby.BLL
+{
+    /// <summary>
+    /// Klasa produktu, zarządza produktami
+    /// </summary>
+    /// 
+
+    public class Produkt
+    {
+        private int produktId;
+
+        public int ProduktId
+        {
+            get { return produktId; }
+            set { produktId = value; }
+        }
+
+        private string nazwaProduktu;
+
+        public string NazwaProduktu
+        {
+            get { return nazwaProduktu; }
+            set { nazwaProduktu = value; }
+        }
+
+        private string opis;
+
+        public string Opis
+        {
+            get { return opis; }
+            set { opis = value; }
+        }
+
+        public string PowiedzWitaj()
+        {
+
+            return "Witaj " + NazwaProduktu + " (" + ProduktId + "): " + Opis;
+        }
+
+
+
+    }
+}
---
+++ DevHobby.BLLTesty/ProduktTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevHobby.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevHobby.BLL.Tests
+{
+    [TestClass()]
+    public class ProduktTests
+    {
+        [TestMethod()]
+        public void PowiedzWitajTest()
+        {
+
+            // Arrange
+            var produkt = new Produkt();
+            
+
+            // Act
+
+
+            // Assert
+
+            Assert.Fail();
+        }
+    }
+}
